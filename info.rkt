bc8140a335ca3d45d35ba7f7135d86aa0167aee4
srfoster <stephen@thoughtstem.com>
2019-10-01 14:59:40 -0700

    define-assets-from dep

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "clicker-assets")
-(define deps '("base"))
+(define deps '("base" "define-assets-from"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/clicker-assets.scrbl" ())))
 (define pkg-desc "Description Here")
